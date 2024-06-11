85b7d57bd4b1c1bbe10ab5da2ee74d97d0983bff
pallabofficial <officialpallabmondal000@gmail.com>
2024-06-11 07:41:11 +0530

    Add react-router-dom and MUI components:- Updated App component with Box from @mui/material and Routes from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+
 import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+        
+    </Box>
   )
 }
 
